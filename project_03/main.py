f7fff47d7674b6ec593e8c4ba8074ac070858b3a
Suraj Kumar <surajkumar09464@gmail.com>
2026-05-03 01:03:53 +0530

    add

--- project_03/main.py
+++ project_03/main.py
@@ -15,4 +15,11 @@ bgLabel.place(x=0,y=0)
 loginFrame = Frame(window)
 loginFrame.place(x=400, y=150)
 
+logoImage = PhotoImage(file='graduated.png')
+
+logoLabel = Label(loginFrame, image=logoImage)
+logoLabel.grid(row=0, column=0)
+
+usernameLabel=Label(loginFrame, image=, text='Username')
+
 window.mainloop()
