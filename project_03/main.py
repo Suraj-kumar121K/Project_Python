0eb520c80bc5562d9b71d3725e3450104c053afc
Suraj Kumar <surajkumar09464@gmail.com>
2026-05-03 00:54:44 +0530

    add

--- project_03/main.py
+++ project_03/main.py
@@ -1,7 +1,18 @@
 from tkinter import *
+from PIL import ImageTk
 
 window = Tk()
 
 window.geometry('1280x700+0+0')
+
+window.resizable(False,False)
+
+backgroundImage = ImageTk.PhotoImage(file='Background_image.jpg')
+
+bgLabel = Label(window, image=backgroundImage)
+bgLabel.place(x=0,y=0)
+
+loginFrame = Frame(window)
+loginFrame.place(x=400, y=150)
 
 window.mainloop()
