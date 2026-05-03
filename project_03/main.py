cab0f1217f7ba05535577e04d62a92dfe596c0c6
Suraj Kumar <surajkumar09464@gmail.com>
2026-05-03 16:46:40 +0530

    add

--- project_03/main.py
+++ project_03/main.py
@@ -7,29 +7,22 @@ window.resizable(False, False)
 
 # ---------------- BACKGROUND ----------------
 backgroundImage = ImageTk.PhotoImage(file='bg.jpg')
-bgLabel = Label(window, image=backgroundImage)
-bgLabel.place(x=0, y=0)
+
+bgLabel=Label(window,image=backgroundImage)
+bgLabel.place(x=0,y=0)
 
 # ---------------- LOGIN FRAME ----------------
-loginFrame = Frame(window, bg="white")
-loginFrame.place(x=500, y=120)
+loginFrame=Frame(window)
+loginFrame.place(x=500, y=150)
 
 # ---------------- LOGO ----------------
-logoImage = PhotoImage(file='graduated.png')
-logoLabel = Label(loginFrame, image=logoImage, bg="white")
-logoLabel.grid(row=0, column=0, columnspan=2, pady=20)
+logoImage = PhotoImage(file='logo.png')
+logoLabel = Label(loginFrame,image=logoImage)
+logoLabel.grid(row=0, column=0)
 
-# ---------------- AVATAR + USERNAME ----------------
-avatarImage = PhotoImage(file='user.png')
-avatarLabel = Label(loginFrame, image=avatarImage, bg="white")
-avatarLabel.grid(row=1, column=0, padx=10, pady=10)
-
-usernameLabel = Label(loginFrame, text="Username",
-                      font=("times new roman", 14, "bold"),
-                      bg="white")
-usernameLabel.grid(row=1, column=1, sticky=W, padx=10)
-
-usernameEntry = Entry(loginFrame, font=("times new roman", 14), bd=2, relief=GROOVE)
-usernameEntry.grid(row=2, column=0, columnspan=2, padx=0, pady=0)
+# ---------------- USERNAME ----------------
+usernameImage = PhotoImage(file='user.png')
+usernameLabel=Label(loginFrame,image=usernameImage, text='Username', compound=LEFT)
+usernameLabel.grid(row=1, column=0)
 
 window.mainloop()
